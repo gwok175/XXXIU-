9d55c146789a192fc3936ad5cb864491320eecc0
gwok175 <woelkegabriel@gmail.com>
2026-03-23 22:13:40 -0300

    Create contacts.mock.js

---
+++ src/styles/src/src/src/lib/src/store/src/router/src/components/router/src/hooks/src/hooks/src/hooks/src/hooks/src/hooks/src/services/src/services/src/services/src/mocks/contacts.mock.js
@@ -0,0 +1,26 @@
+export const contactsMock = [
+  {
+    id: '1',
+    nickname: 'Agente K',
+    status: 'online',
+    avatar_url: null,
+    owner_email: 'user@void.net',
+    contact_email: 'agente.k@void.net',
+  },
+  {
+    id: '2',
+    nickname: 'Sombra',
+    status: 'offline',
+    avatar_url: null,
+    owner_email: 'user@void.net',
+    contact_email: 'sombra@void.net',
+  },
+  {
+    id: '3',
+    nickname: 'Corvo',
+    status: 'online',
+    avatar_url: null,
+    owner_email: 'user@void.net',
+    contact_email: 'corvo@void.net',
+  },
+]
